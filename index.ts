956216afc4dae692e31452a23335de2fb582d86b
azabroflovski <azabroflovski@gmail.com>
2024-09-26 12:33:47 +0500

    refactor: add success message

--- index.ts
+++ index.ts
@@ -3,15 +3,17 @@ import { faker } from '@faker-js/faker';
 import { logDB } from './logdb.ts'
 
 const schedule = scheduleJob('*/5 * * * * *', async () => {
+    console.log('hr://birthday', 'payload:', {
+        fullName: faker.person.fullName(),
+        email: faker.internet.email(),
+    })
+
     await logDB.sendEvent('birthday', {
         fullName: faker.person.fullName(),
         email: faker.internet.email(),
     })
 
-    console.log('hr://birthday', 'payload:', {
-        fullName: faker.person.fullName(),
-        email: faker.internet.email(),
-    })
+    console.log('Event stored')
 })
 
 console.log('Schedule running: */5 * * * * *')
