072c03baa47e8328b705fba6bf65ef0c6fd1040e
azabroflovski <azabroflovski@gmail.com>
2024-09-26 11:44:34 +0500

    init

---
+++ index.ts
@@ -0,0 +1,12 @@
+import { scheduleJob } from 'node-schedule'
+import { faker } from '@faker-js/faker';
+import { logDB } from './logdb.ts'
+
+const schedule = scheduleJob('*/5 * * * *', async () => {
+    await logDB.sendEvent('birthday', {
+        fullName: faker.person.fullName(),
+        email: faker.internet.email(),
+    })
+})
+
+console.log(schedule, 'running')
---
+++ logdb.ts
@@ -0,0 +1,6 @@
+import { LogDB } from 'logdb-client'
+export const logDB = new LogDB({
+    name: 'hr',
+    url: '...',
+    token: '...'
+})
