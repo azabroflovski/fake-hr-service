f1fa854cfdb8135d6ad7900a615987b553ffed6f
azabroflovski <azabroflovski@gmail.com>
2024-09-26 15:50:49 +0500

    fix: different user name in log

--- index.ts
+++ index.ts
@@ -3,10 +3,12 @@ import { faker } from '@faker-js/faker';
 import { logDB } from './logdb.ts'
 
 const schedule = scheduleJob('*/5 * * * * *', async () => {
-    console.log('hr://birthday', 'payload', JSON.stringify({
+    const fakeUser = {
         fullName: faker.person.fullName(),
         email: faker.internet.email(),
-    }))
+    }
+
+    console.log('hr://birthday', 'payload', JSON.stringify(fakeUser))
 
     await logDB.sendEvent('birthday', {
         fullName: faker.person.fullName(),
