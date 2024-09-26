3b44edce3ff3d40286514f12fc51f5a37e67094a
azabroflovski <azabroflovski@gmail.com>
2024-09-26 14:47:10 +0500

    fix: update logdb url

--- logdb.ts
+++ logdb.ts
@@ -1,6 +1,6 @@
 import { LogDB } from 'logdb-client'
 export const logDB = new LogDB({
     name: 'hr',
-    url: 'https://dev.asys.xt-xarid.uz/logdb',
+    url: 'http://logdbphp-nginx',
     token: 'ik722LlhGhGLyGvt7ApZj9IdLyephZ5M7wYsJzjJ',
 })
