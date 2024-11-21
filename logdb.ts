ff2aff0858b684dd2cc3f6cf241989963c7d5fd6
azabroflovski <azabroflovski@gmail.com>
2024-11-21 11:35:11 +0500

    chore: update env

--- logdb.ts
+++ logdb.ts
@@ -2,6 +2,6 @@ import { LogDB } from 'logdb-client'
 
 export const logDB = new LogDB({
     name: 'hr',
-    url: 'http://logdbphp-nginx',
-    token: 'eO5eivhJNPZ9laLnC6kAlf8HNooD4VXFvRTW1S0G',
+    url: 'http://logdbphp.test/api/v2',
+    token: '5ecOLcL5Gd18w3hIUnef57PTbpBWEhHyV1xlalEl',
 })
